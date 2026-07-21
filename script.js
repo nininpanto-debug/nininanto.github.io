6ebd0eaf81c373483c811c345b090467d2fc41e9
Ninin Anto <nininpanto@gmail.com>
2026-07-21 14:52:37 +0530

    adding animation

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 function enterWorld() {
+
         const intro = document.getElementById("intro");
             const main = document.getElementById("main");
 
                 intro.style.opacity = "0";
-                    intro.style.transition = "1s";
+                    intro.style.transform = "scale(1.08)";
 
                         setTimeout(() => {
                                 intro.style.display = "none";
                                         main.classList.remove("hidden");
                                                 main.classList.add("show");
                                                     }, 1000);
+
                                                     }
